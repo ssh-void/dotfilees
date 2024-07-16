087e822acdefc8e7050cdb81d5738d77564791a2
ssh-void <168687039+ssh-void@users.noreply.github.com>
2024-07-16 02:12:26 +0100

    Update pipewire.sh

--- pipewire.sh
+++ pipewire.sh
@@ -1,6 +1,6 @@
 sudo xbps-install -Syu pipewire pamixer alsa-pipewire pipewire-devel libjack-pipewire wireplumber wireplumber-devel pavucontrol  
 # active
-sudo ln -s /etc/sv/dbus /var/service/
+sudo ln -s /etc/sv/dbus/ /var/service/
 sudo mkdir -p /etc/pipewire/pipewire.conf.d
 sudo ln -s /usr/share/examples/wireplumber/10-wireplumber.conf /etc/pipewire/pipewire.conf.d/
 sudo mkdir -p /etc/alsa/conf.d
@@ -9,5 +9,5 @@ sudo ln -s /usr/share/alsa/alsa.conf.d/99-pipewire-default.conf /etc/alsa/conf.d
 # bluetooth
 sudo xbps-install -Syu rfkill util-linux bluez libspa-bluetooth blueman
 sudo rfkill unblock bluetooth
-sudo ln -s /etc/sv/bluetoothd /var/service/
+sudo ln -s /etc/sv/bluetoothd/ /var/service/
 sudo  shutdown -r now
