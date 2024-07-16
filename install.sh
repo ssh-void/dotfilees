49eca19e36a6408cb30d58c18064c436d1f61e3b
ssh-void <168687039+ssh-void@users.noreply.github.com>
2024-07-16 19:58:51 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -31,15 +31,6 @@ clear
 # bluetooth && pipewire		
 sudo xbps-install -Syu rfkill util-linux bluez libspa-bluetooth blueman
 
-clear
-
-# active
-sudo ln -s /etc/sv/dbus /var/service/
-sudo mkdir -p /etc/pipewire/pipewire.conf.d
-sudo ln -s /usr/share/examples/wireplumber/10-wireplumber.conf /etc/pipewire/pipewire.conf.d/
-sudo mkdir -p /etc/alsa/conf.d
-sudo ln -s /usr/share/alsa/alsa.conf.d/50-pipewire.conf /etc/alsa/conf.d
-sudo ln -s /usr/share/alsa/alsa.conf.d/99-pipewire-default.conf /etc/alsa/conf.d
 
 clear
 
