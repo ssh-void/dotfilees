efbbd428e7596e44c992e52a69acdee8cf9c2a02
ssh-void <168687039+ssh-void@users.noreply.github.com>
2024-08-03 00:40:51 +0100

    Update kvm.sh

--- kvm.sh
+++ kvm.sh
@@ -1,14 +1,14 @@
-doas xbps-install -Syu virt-manager libvirt qemu openbsd-netcat dnsmasq vde2 bridge-utils
-doas usermod -a -G libvirt $(whoami) 
-doas modprobe kvm-intel  
-doas usermod -a -G libvirt $(whoami)  
-doas sed -i 's/auth_unix_ro = "polkit"/auth_unix_ro = "none"/' /etc/libvirt/libvirtd.conf  # tested
-doas sed -i 's/auth_unix_rw = "polkit"/auth_unix_rw = "none"/' /etc/libvirt/libvirtd.conf # tested
-doas sed -i 's/user = ".*"/user = "'"$(whoami)"'"/' /etc/libvirt/qemu.conf # tested
-doas xbps-install -Syu NetworkManager
-doas sv down dhcpcd # runit
-doas rm /var/service/dhcpcd
-doas ln -s /etc/sv/NetworkManager /var/service/
-doas ln -s /etc/sv/dbus /var/service/
-#doas reboot
-#doas nmcli device wifi connect SSID password PASSWORD
+sudo xbps-install -Syu virt-manager libvirt qemu openbsd-netcat dnsmasq vde2 bridge-utils
+sudo usermod -a -G libvirt $(whoami) 
+sudo modprobe kvm-intel  
+sudo usermod -a -G libvirt $(whoami)  
+sudo sed -i 's/auth_unix_ro = "polkit"/auth_unix_ro = "none"/' /etc/libvirt/libvirtd.conf  # tested
+sudo sed -i 's/auth_unix_rw = "polkit"/auth_unix_rw = "none"/' /etc/libvirt/libvirtd.conf # tested
+sudo sed -i 's/user = ".*"/user = "'"$(whoami)"'"/' /etc/libvirt/qemu.conf # tested
+sudo xbps-install -Syu NetworkManager
+sudo sv down dhcpcd # runit
+sudo rm /var/service/dhcpcd
+sudo ln -s /etc/sv/NetworkManager /var/service/
+sudo ln -s /etc/sv/dbus /var/service/
+#sudo nmcli device wifi connect SSID password PASSWORD
+#sudo reboot
