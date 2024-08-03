e0e973213b588f29dfa567f8effe319d4dff18d6
ssh-void <168687039+ssh-void@users.noreply.github.com>
2024-08-03 02:14:26 +0100

    Update xbps-src.sh

--- xbps-src.sh
+++ xbps-src.sh
@@ -1,11 +1,15 @@
-sudo xbps-install -Sy xtools
+#sudo xbps-install -Syu xtools git
 #=============================================================#
-./xbps-src binary-bootstrap
-sudo echo XBPS_ALLOW_RESTRICTED=yes >> etc/conf
+cd /opt/
+#sudo chown -R $(whoami):$(whoami) .
+cd /opt/void-packages/
+git clean -fd && git reset --hard && git pull
+#=============================================================#
+./xbps-src binary-bootstrap # xbps-src cannot be used as root
+echo XBPS_ALLOW_RESTRICTED=yes >> etc/conf
 #=============================================================#
 ./xbps-src pkg torbrowser-launcher && xi torbrowser-launcher
-./xbps-src pkg google-chrome &&  xi google-chrome
-./xbps-src pkg discord && xi discord
-./xbps-src bootstrap-update
-./xbps-src update-sys
+#./xbps-src pkg google-chrome &&  xi google-chrome
+#./xbps-src pkg discord && xi discord
+./xbps-src bootstrap-update && ./xbps-src update-sys
 ./xbps-src clean-repocache
