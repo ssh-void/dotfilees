4627410e8d48d2ab88a21096193ec9d38d0979e1
ssh-void <168687039+ssh-void@users.noreply.github.com>
2024-08-04 02:10:40 +0100

    Create  scratchpad.sh

---
+++ .config/.script/scratchpad.sh
@@ -0,0 +1,10 @@
+winclass="$(xdotool search --classname scpad)" # i xdo xdotool
+if [ -z "$winclass" ]; then
+	st -f "Ubuntu Mono:size=16" -g "100x30" -n "scpad" &> /dev/null & 	
+else
+    if [ ! -f /tmp/scpad ]; then
+	touch /tmp/scpad && xdo hide "$winclass"
+    elif [ -f /tmp/scpad ]; then
+	rm /tmp/scpad && xdo show "$winclass"
+    fi
+fi
