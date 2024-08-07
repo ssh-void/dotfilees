60a2a50a35852a39c03c5120da5220091995b7e7
ssh-void <168687039+ssh-void@users.noreply.github.com>
2024-08-07 23:08:11 +0100

    Update xrandrr.sh

--- xrandrr.sh
+++ xrandrr.sh
@@ -1,2 +1,9 @@
-#!/bin/sh
-xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x1920 --rotate normal --output HDMI-1 --mode 1920x1080 --pos 1417x0 --rotate right  --rate 50 --output DP-1 --off --output HDMI-2 --off
+## xrandr
+xrandr \
+    --dpi 96 \
+    --output HDMI1  --mode 1920x1080 --pos 0x1920 --rotate normal --rate 60 --brightness 1 \
+    --mode 1920x1080 --pos 1417x0 --rotate right --rate 50 --brightness 0.8 \
+    --output HDMI2 --off \
+    --output DP1 --off \
+    --output HDMI2 --off  \
+    --output VIRTUAL1 --off
