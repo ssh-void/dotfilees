a85fd0608b8abd32daa642b4b844cf32a84759dc
ssh-void <168687039+ssh-void@users.noreply.github.com>
2024-08-07 01:50:55 +0100

    Add files via upload

---
+++ xrandrr.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x1920 --rotate normal --output HDMI-1 --mode 1920x1080 --pos 1417x0 --rotate right  --rate 50 --output DP-1 --off --output HDMI-2 --off
