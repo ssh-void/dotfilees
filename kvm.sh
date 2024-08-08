a4c3b69f2f813883fea100cae4654370d52923f6
ssh-void <168687039+ssh-void@users.noreply.github.com>
2024-08-08 15:06:25 +0100

    Update kvm.sh

--- kvm.sh
+++ kvm.sh
@@ -1,11 +1,10 @@
-sudo xbps-install -Syu virt-manager libvirt qemu openbsd-netcat dnsmasq vde2 bridge-utils spice-vdagent
+sudo xbps-install -Syu NetworkManager virt-manager libvirt qemu openbsd-netcat dnsmasq vde2 bridge-utils spice-vdagent
 sudo usermod -aG kvm $(whoami) 
 sudo modprobe kvm-intel  
 sudo usermod -aG libvirt $(whoami) 
 sudo sed -i 's/^#auth_unix_ro = "polkit"/auth_unix_ro = "none"/' /etc/libvirt/libvirtd.conf # tested
 sudo sed -i 's/^#auth_unix_rw = "polkit"/auth_unix_rw = "none"/' /etc/libvirt/libvirtd.conf # tested
-sudo sed -i 's/user = ".*"/user = "'"$(whoami)"'"/' /etc/libvirt/qemu.conf # tested
-sudo xbps-install -Syu NetworkManager
+sudo sed -i 's/^#\?user = ".*"/user = "'"$(whoami)"'"/' /etc/libvirt/qemu.conf  # tested
 sudo sv down dhcpcd # runit
 sudo rm /var/service/dhcpcd
 sudo ln -s /etc/sv/NetworkManager /var/service/
