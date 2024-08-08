d0882d8db365bcd6c1aad7c24853bf887e7d962f
ssh-void <168687039+ssh-void@users.noreply.github.com>
2024-08-08 13:46:48 +0100

    Update kvm.sh

--- kvm.sh
+++ kvm.sh
@@ -1,7 +1,6 @@
 sudo xbps-install -Syu virt-manager libvirt qemu openbsd-netcat dnsmasq vde2 bridge-utils spice-vdagent
 sudo usermod -a -G libvirt $(whoami) 
 sudo modprobe kvm-intel  
-sudo usermod -a -G libvirt $(whoami)  
 sudo sed -i 's/auth_unix_ro = "polkit"/auth_unix_ro = "none"/' /etc/libvirt/libvirtd.conf  # tested
 sudo sed -i 's/auth_unix_rw = "polkit"/auth_unix_rw = "none"/' /etc/libvirt/libvirtd.conf # tested
 sudo sed -i 's/user = ".*"/user = "'"$(whoami)"'"/' /etc/libvirt/qemu.conf # tested
