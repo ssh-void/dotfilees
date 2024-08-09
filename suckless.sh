991bb8d5662bf1a28703dccf3e68eb700a5f80c3
ssh-void <168687039+ssh-void@users.noreply.github.com>
2024-08-09 19:03:40 +0100

    Update suckless.sh

--- suckless.sh
+++ suckless.sh
@@ -2,3 +2,10 @@ sudo xbps-install -S && sudo xbps-install -u xbps && sudo xbps-install -Syu
 sudo xbps-install -Syu xorg-minimal base-devel xset xrandr arandr xdotool xdo xrdb xf86-input-synaptics xf86-input-evdev 
 sudo xbps-install -Syu ttf-ubuntu-font-family dejavu-fonts-ttf noto-fonts-emoji noto-fonts-cjk libinput-gestures libX11-devel libXft-devel libXinerama-devel fribidi-devel harfbuzz-devel libXrandr-devel xf86-input-synaptics xf86-input-evdev 
 sudo xbps-install -Syu lm_sensors tree man man-db fzy inxi flameshot maim xclip cloc awk	
+sudo sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/' /etc/default/grub
+sudo grub-mkconfig -o /boot/grub/grub.cfg
+sudo shutdown -r now # reboot   
+sudo rm /var/service/agetty-tty3
+sudo rm /var/service/agetty-tty4
+sudo rm /var/service/agetty-tty5
+sudo rm /var/service/agetty-tty6
