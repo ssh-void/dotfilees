ac64c55133c808e09a7dccfbc4203092b0e03a5b
ssh-void <168687039+ssh-void@users.noreply.github.com>
2024-08-09 20:08:07 +0100

    Update pamixer.sh

--- .config/.script/pamixer.sh
+++ .config/.script/pamixer.sh
@@ -2,15 +2,15 @@
 case "$1" in
   up)
     # +5
-    pamixer -u && pamixer --increase  5 > /dev/null 2>&1 
+    pamixer -u && pamixer --increase  5 &> /dev/null 
     ;;
   down)
     # -5
-   pamixer -u && pamixer --decrease 5 > /dev/null 2>&1 
+   pamixer -u && pamixer --decrease 5 &> /dev/null
     ;;
   toggle)
     # mute
-    pamixer --toggle-mute > /dev/null 2>&1 
+    pamixer --toggle-mute &> /dev/null
     ;;
   *)
     echo "Usage: $0 {up|down|toggle}"
