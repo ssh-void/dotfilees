497b9422471bfb7be33515b7b247b5b835b93fc5
ssh-void <168687039+ssh-void@users.noreply.github.com>
2024-08-09 16:58:06 +0100

    Create suckless.sh

---
+++ suckless.sh
@@ -0,0 +1,4 @@
+sudo xbps-install -S && sudo xbps-install -u xbps && sudo xbps-install -Syu
+sudo xbps-install -Syu xorg-minimal base-devel xset xrandr arandr xdotool xdo xrdb xf86-input-synaptics xf86-input-evdev 
+sudo xbps-install -Syu ttf-ubuntu-font-family dejavu-fonts-ttf noto-fonts-emoji noto-fonts-cjk libinput-gestures libX11-devel libXft-devel libXinerama-devel fribidi-devel harfbuzz-devel libXrandr-devel xf86-input-synaptics xf86-input-evdev 
+sudo xbps-install -Syu lm_sensors tree man man-db fzy inxi flameshot maim xclip cloc awk	
