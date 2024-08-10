c0704411dcd6d627519602cbd6ee5f8a8aebb3f2
ssh-void <168687039+ssh-void@users.noreply.github.com>
2024-08-10 18:22:26 +0100

    Update suckless.sh

--- suckless.sh
+++ suckless.sh
@@ -1,8 +1,8 @@
 sudo xbps-install -S && sudo xbps-install -u xbps && sudo xbps-install -Syu
 sudo xbps-install -Syu wget picom
 sudo xbps-install -Syu xorg-minimal xrdb base-devel xset xrandr arandr xdotool xdo xrdb xf86-input-synaptics xf86-input-evdev 
-sudo xbps-install -Syu ttf-ubuntu-font-family dejavu-fonts-ttf noto-fonts-emoji noto-fonts-cjk libinput-gestures libX11-devel libXft-devel libXinerama-devel fribidi-devel harfbuzz-devel libXrandr-devel xf86-input-synaptics xf86-input-evdev 
-sudo xbps-install -Syu lm_sensors tree man man-db fzy inxi flameshot maim xclip cloc awk dbus picom sxhkd xbanish feh brightnessctl numlockx
+sudo xbps-install -Syu ttf-ubuntu-font-family dejavu-fonts-ttf noto-fonts-emoji noto-fonts-cjk libinput-gestures libX11-devel libXft-devel libXinerama-devel fribidi-devel harfbuzz-devel libXrandr-devel 
+sudo xbps-install -Syu lm_sensors tree man man-db fzy inxi flameshot maim xclip cloc awk dbus picom sxhkd xbanish feh brightnessctl numlockx firefox eza most  
 sudo sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/' /etc/default/grub
 sudo grub-mkconfig -o /boot/grub/grub.cfg
 sudo shutdown -r now # reboot   
@@ -10,6 +10,8 @@ sudo rm /var/service/agetty-tty3
 sudo rm /var/service/agetty-tty4
 sudo rm /var/service/agetty-tty5
 sudo rm /var/service/agetty-tty6
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 cd ~ && wget -c https://raw.githubusercontent.com/ssh-void/dotfilees/main/.bashrc
 cd ~ && wget -c https://raw.githubusercontent.com/ssh-void/dotfilees/main/.bash_profile
 cd ~ && wget -c https://raw.githubusercontent.com/ssh-void/dotfilees/main/.profile
@@ -23,5 +25,15 @@ sudo cp ~/.profile /root/.profile
 sudo cp ~/.bash_profile /root/.bash_profile
 sudo mkdir -p /root/.config/nvim
 sudo cp ~/.config/nvin/init.vin /root/.config/nvin/init.vin
-
 sudo echo "permit nopass $(whoami) as root" > /etc/doas.conf
+sudo ln -s /usr/share/fontconfig/conf.avail/70-no-bitmaps.conf /etc/fonts/conf.d/
+sudo xbps-reconfigure -f fontconfig
+cd /opt/
+sudo git clone --depth=1 https://github.com/garabik/grc.git
+sudo chown -R  $(whoami): $(whoami) .
+cd grc/
+sudo ./install.sh
+sudo cp /etc/profile.d/grc.sh /etc/
+####################################################################
+sudo xbps-reconfigure -fa
+fc-cache -fv
