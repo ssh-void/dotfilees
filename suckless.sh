ff8beb2fb075474f152461ad14a3d5fdeda18d87
ssh-void <168687039+ssh-void@users.noreply.github.com>
2024-08-10 20:45:14 +0100

    Update suckless.sh

--- suckless.sh
+++ suckless.sh
@@ -1,5 +1,5 @@
 sudo xbps-install -S && sudo xbps-install -u xbps && sudo xbps-install -Syu
-sudo xbps-install -Syu wget picom
+sudo xbps-install -Syu wget picom opendoas
 sudo xbps-install -Syu xorg-minimal xrdb base-devel xset xrandr arandr xdotool xdo xrdb xf86-input-synaptics xf86-input-evdev 
 sudo xbps-install -Syu ttf-ubuntu-font-family dejavu-fonts-ttf noto-fonts-emoji noto-fonts-cjk libinput-gestures libX11-devel libXft-devel libXinerama-devel fribidi-devel harfbuzz-devel libXrandr-devel 
 sudo xbps-install -Syu lm_sensors tree man man-db fzy inxi flameshot maim xclip cloc awk dbus picom sxhkd xbanish feh brightnessctl numlockx firefox eza most  
