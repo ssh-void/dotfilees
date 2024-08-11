3494bdea32ea610f91dc30d87fe3116bcfd6c84d
ssh-void <168687039+ssh-void@users.noreply.github.com>
2024-08-11 04:59:42 +0100

    Update suckless.sh

--- suckless.sh
+++ suckless.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 
-set -e  # Arrêter le script en cas d'erreur
-
 # Mettre à jour le système et les outils
-echo "Mise à jour du système et des outils..."
-sudo xbps-install -S
-sudo xbps-install -u xbps
-sudo xbps-install -Syu
+sudo xbps-install -Syu xbps
+if [ $? -ne 0 ]; then echo "Erreur lors de la mise à jour de xbps."; exit 1; fi
 
 # Installer les paquets nécessaires
-echo "Installation des paquets nécessaires..."
 sudo xbps-install -Syu \
     xorg-minimal xrdb base-devel xset xrandr arandr xdotool xdo xf86-input-synaptics xf86-input-evdev \
     bash-completion ttf-ubuntu-font-family dejavu-fonts-ttf noto-fonts-emoji noto-fonts-cjk \
     libinput-gestures libX11-devel libXft-devel libXinerama-devel fribidi-devel harfbuzz-devel \
     libXrandr-devel lm_sensors tree man man-db fzy inxi flameshot xclip awk dbus wget curl picom \
     sxhkd xbanish feh brightnessctl numlockx firefox eza most git
+if [ $? -ne 0 ]; then echo "Erreur lors de l'installation des paquets."; exit 1; fi
 
 # Télécharger et configurer les fichiers de configuration
 BASE_URL="https://raw.githubusercontent.com/ssh-void/dotfilees/main"
@@ -30,8 +26,6 @@ FILES=(
     ".Xresources"
     ".config/sh.png"
 )
-
-echo "Téléchargement des fichiers de configuration..."
 for FILE in "${FILES[@]}"; do
     wget -c "$BASE_URL/$FILE" -O "$HOME/$FILE"
     if [ $? -ne 0 ]; then echo "Erreur lors du téléchargement de $FILE."; exit 1; fi
@@ -44,14 +38,13 @@ sudo cp $HOME/.bash_profile /root/.bash_profile
 sudo cp $HOME/.config/nvim/init.vim /root/.config/nvim/init.vim
 
 # Installer vim-plug
-echo "Installation de vim-plug..."
 sudo xbps-install -Syu curl
+if [ $? -ne 0 ]; then echo "Erreur lors de l'installation de curl."; exit 1; fi
 curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-if [ $? -ne 0 ]; then echo "Erreur lors de l'installation de vim-plug."; exit 1; fi
+if [ $? -ne 0 ]; then echo "Erreur lors du téléchargement de vim-plug."; exit 1; fi
 
 # Configurer les fichiers Xorg
-echo "Configuration des fichiers Xorg..."
 XORG_CONF_URLS=(
     "00-keyboard.conf"
     "10-evdev.conf"
@@ -65,7 +58,6 @@ for CONF in "${XORG_CONF_URLS[@]}"; do
 done
 
 # Cloner et installer les outils suckless
-echo "Installation des outils suckless..."
 SUCKLESS_REPO="https://github.com/ssh-void/suckless6.5.git"
 mkdir -p $HOME/.config/.suckless
 git clone $SUCKLESS_REPO $HOME/.config/.suckless
@@ -79,18 +71,16 @@ for DIR in "${SUCKLESS_DIRS[@]}"; do
 done
 
 # Configurer doas
-echo "Configuration de doas..."
 sudo xbps-install -Syu opendoas
+if [ $? -ne 0 ]; then echo "Erreur lors de l'installation de opendoas."; exit 1; fi
 echo "permit nopass $(whoami) as root" | sudo tee /etc/doas.conf > /dev/null
 if [ $? -ne 0 ]; then echo "Erreur lors de la configuration de doas."; exit 1; fi
 
 # Configurer les polices
-echo "Configuration des polices..."
 sudo ln -s /usr/share/fontconfig/conf.avail/70-no-bitmaps.conf /etc/fonts/conf.d/
 sudo xbps-reconfigure -fa && fc-cache -fv && sudo xbps-reconfigure -f fontconfig
 
 # Installer et configurer grc
-echo "Installation et configuration de grc..."
 cd /opt/
 sudo git clone --depth=1 https://github.com/garabik/grc.git
 sudo git clone --depth=1 https://github.com/void-linux/void-packages
@@ -109,7 +99,6 @@ sudo xbps-reconfigure -fa && sudo update-grub
 if [ $? -ne 0 ]; then echo "Erreur lors de la configuration de grub."; exit 1; fi
 
 # Configurer grub et agetty
-echo "Configuration de grub et agetty..."
 sudo sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/' /etc/default/grub
 sudo sed -i 's/GETTY_ARGS="--noclear"/GETTY_ARGS="--noclear --autologin $(whoami)"/' /etc/runit/runsvdir/current/agetty-tty1/conf
 
@@ -120,5 +109,4 @@ if [ $? -ne 0 ]; then echo "Erreur lors de la mise à jour de grub."; exit 1; fi
 sudo rm -f /var/service/agetty-tty{3,4,5,6}
 
 # Redémarrer le système
-echo "Redémarrage du système..."
 sudo shutdown -r now
