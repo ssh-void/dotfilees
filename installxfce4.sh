9d957bf86fb4f301c621eaa9fedc5fe1b067ceb7
ssh-void <168687039+ssh-void@users.noreply.github.com>
2024-09-17 22:39:48 +0100

    Update installxfce4.sh

--- installxfce4.sh
+++ installxfce4.sh
@@ -14,7 +14,7 @@ clear
 echo "Updating The System"
 sudo xbps-install -S && sudo xbps-install -u xbps && sudo xbps-install -Syu
 clear
-sudo xbps-install -Syu xorg-minimal base-devel xset xrandr arandr xdotool xdo xrdb xf86-input-synaptics xf86-input-evdev lm_sensors tree man man-db fzy inxi flameshot maim xclip cloc awk neovim picom dnsmasq openresolv tealdeer elogind fastfetch most sxhkd aria2 git htop opendoas base-devel tmux sxiv feh yt-dlp ffmpeg alsa-plugins-ffmpeg nv-codec-headers xrdb dunst notify-send.sh numlockx bash-completion gcolor2 colordiff brightnessctl xsetroot xbanish yaru lxappearance qt5ct ttf-ubuntu-font-family noto-fonts-emoji noto-fonts-cjk dbus libinput-gestures libX11-devel libXft-devel libXinerama-devel fribidi-devel harfbuzz-devel libXrandr-devel 
+sudo xbps-install -Syu xorg-minimal base-devel xset xrandr arandr xdotool xdo xrdb xf86-input-synaptics xf86-input-evdev lm_sensors tree man man-db fzy inxi flameshot maim xclip cloc awk neovim picom tealdeer elogind fastfetch most sxhkd aria2 git htop opendoas base-devel tmux sxiv feh yt-dlp ffmpeg alsa-plugins-ffmpeg nv-codec-headers xrdb numlockx bash-completion gcolor2 colordiff xsetroot yaru lxappearance qt5ct ttf-ubuntu-font-family noto-fonts-emoji noto-fonts-cjk dbus libinput-gestures libX11-devel libXft-devel libXinerama-devel fribidi-devel harfbuzz-devel libXrandr-devel 
 mkdir -p $HOME/Downloads/{Aria2c,Qbittorrent,Firefox,Brave,yt-dlp/{audio,video}}
 
 # You install the following package 
@@ -27,7 +27,6 @@ mkdir -p $HOME/Downloads/{Aria2c,Qbittorrent,Firefox,Brave,yt-dlp/{audio,video}}
 clear
 sudo xbps-install -Syu socklog ufw gufw apparmor # seurtié
 sudo xbps-install -Syu tesseract-ocr tesseract-ocr-eng tesseract-ocr-ara  
-sudo xbps-install -Syu wmname # contourner certains bogues liés aux gestionnaires de fenêtres. 
 sudo xbps-install -Syu net-tools NetworkManager # ifconfig netstat .. 
 clear
 
@@ -45,7 +44,7 @@ clear
 #sudo xbps-install -Syu kdenlive krita  # video editor
 
 # firefox
-sudo xbps-install -Syu firefox speech-dispatcher firefox-i18n-en-US firefox-i18n-ar firefox-i18n-fr font-sil-doulos font-liberation-narrow-ttf
+sudo xbps-install -Syu firefox speech-dispatcher firefox-i18n-en-US firefox-i18n-ar firefox-i18n-fr font-sil-doulos font-liberation-narrow-ttf ntfs-3g exfat-utils
 sudo xbps-install -Syu tor obfs4proxy torsocks # w3m w3m-img # tor
 sudo xbps-install -Syu qbittorrent
 sudo xbps-install -Syu duf eza # lsd exa 
@@ -55,9 +54,9 @@ sudo ln -s /usr/share/fontconfig/conf.avail/70-no-bitmaps.conf /etc/fonts/conf.d
 
 clear
 echo "===== pdf ===== "
-sudo xbps-install -Syu zathura zathura-pdf-mupdf ntfs-3g exfat-utils
+sudo xbps-install -Syu zathura zathura-pdf-mupdf 
 # non free répo
-sudo xbps-install -S void-repo-nonfree # add void-repo-multilib-nonfree
+sudo xbps-install -Syu void-repo-nonfree # add void-repo-multilib-nonfree
 xbps-query -L
 
 clear
@@ -102,4 +101,4 @@ sudo rm /var/service/dhcpcd
 sudo sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/' /etc/default/grub
 sudo grub-mkconfig -o /boot/grub/grub.cfg
 sudo update-grub
-sudo shutdown -r now # reboot   
+#sudo shutdown -r now # reboot   
