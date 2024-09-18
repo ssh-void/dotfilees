1a1ebe5047e5fa10bb5da83ebd150c3005d145c0
ssh-void <168687039+ssh-void@users.noreply.github.com>
2024-09-18 01:22:16 +0100

    Update installxfce4baseiso.sh

--- installxfce4baseiso.sh
+++ installxfce4baseiso.sh
@@ -36,8 +36,19 @@ clear
 
 echo "Installazione PIPEWIRE & ALSA..."
 sleep 3s
-	sudo xbps-install -Sy pipewire alsa-pipewire alsa-utils rtkit
-
+       sudo xbps-install -Syu util-linux pipewire pamixer alsa-pipewire pipewire-devel libjack-pipewire wireplumber wireplumber-devel pavucontrol  
+       # active
+       sudo ln -s /etc/sv/dbus/ /var/service/
+       sudo mkdir -p /etc/pipewire/pipewire.conf.d
+       sudo ln -s /usr/share/examples/wireplumber/10-wireplumber.conf /etc/pipewire/pipewire.conf.d/
+       sudo mkdir -p /etc/alsa/conf.d
+       sudo ln -s /usr/share/alsa/alsa.conf.d/50-pipewire.conf /etc/alsa/conf.d
+       sudo ln -s /usr/share/alsa/alsa.conf.d/99-pipewire-default.conf /etc/alsa/conf.d
+       # bluetooth
+       sudo xbps-install -Syu rfkill util-linux bluez libspa-bluetooth blueman
+       sudo rfkill unblock bluetooth
+       sudo ln -s /etc/sv/bluetoothd/ /var/service/
+       #sudo shutdown -r now
 clear
 
 echo "Installazione LIGHTDM & XFCE..."
