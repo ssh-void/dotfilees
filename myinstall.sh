64e50ccf52f35959ebf264d07219233b7fc578a2
ssh-void <168687039+ssh-void@users.noreply.github.com>
2024-09-24 12:54:57 +0100

    Update myinstall.sh

--- myinstall.sh
+++ myinstall.sh
@@ -33,7 +33,8 @@ clear
 sudo xbps-install -Syu dejavu-fonts-ttf noto-fonts-ttf # terminus-font cantarell-fonts noto-fonts-ttf fonts-roboto-ttf dejavu-fonts-ttf ttf-ubuntu-font-family
 sudo xbps-install -Syu vscode eclipse 
 sudo xbps-install -Syu mpv python3-devel python3-pip  
-sudo xbps-install -Syu megatools 
+sudo xbps-install -Syu megatools # mega
+sudo xbps-install -Syu libselinux # matlab 
 # KVM & qemu...
 sudo xbps-install -Syu virt-manager libvirt qemu openbsd-netcat dnsmasq vde2 bridge-utils spice-vdagent
 # tar
