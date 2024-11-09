1fce96802c802be5817786255b420bae1c0fcc4c
ssh-void <168687039+ssh-void@users.noreply.github.com>
2024-11-09 17:48:16 +0100

    Update myinstall.sh

--- myinstall.sh
+++ myinstall.sh
@@ -19,7 +19,7 @@ sudo xbps-install -S && sudo xbps-install -u xbps && sudo xbps-install -Syu
 sudo xbps-install -Syu umbrello # uml systeme
 sudo xbps-install -Syu alacritty telegram-desktop pnpm # current
 clear
-sudo xbps-install -Syu xorg-minimal xorg-server-devel xorg-fonts pkg-config base-devel gcc gdb xset xrandr arandr xdotool xdo xrdb xf86-input-synaptics xf86-input-evdev lm_sensors tree man man-db fzy inxi flameshot maim xclip cloc awk neovim picom dnsmasq openresolv tealdeer fastfetch most sxhkd  htop opendoas base-devel tmux sxiv ffmpeg alsa-plugins-ffmpeg nv-codec-headers xrdb dunst notify-send.sh numlockx bash-completion gcolor2 colordiff brightnessctl xsetroot xbanish yaru lxappearance qt5ct qt6ct 
+sudo xbps-install -Syu xorg-minimal xorg-server-devel xorg-fonts pkg-config base-devel gcc gdb xset xrandr arandr xdotool xdo xrdb xf86-input-synaptics xf86-input-evdev lm_sensors tree man man-db fzy inxi flameshot maim xclip cloc awk neovim picom dnsmasq openresolv tealdeer fastfetch most sxhkd htop opendoas base-devel tmux sxiv ffmpeg alsa-plugins-ffmpeg nv-codec-headers xrdb dunst notify-send.sh numlockx bash-completion readline gcolor2 colordiff brightnessctl xsetroot xbanish yaru lxappearance qt5ct qt6ct 
 sudo xbps-install -Syu ttf-ubuntu-font-family noto-fonts-emoji noto-fonts-cjk dbus python3-dbus libinput-gestures libX11-devel libXft-devel libXinerama-devel fribidi-devel harfbuzz-devel libXrandr-devel 
 sudo xbps-install -Syu fontmanager 
 sudo xbps-install -Syu yt-dlp aria2 wget curl git patch rsync 
