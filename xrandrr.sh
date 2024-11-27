6c99988f2d023cbb204d184ce6c749fd68940199
ssh-void <168687039+ssh-void@users.noreply.github.com>
2024-11-27 17:39:38 +0100

    Update xrandrr.sh

--- xrandrr.sh
+++ xrandrr.sh
@@ -1,9 +1,15 @@
-## xrandr
-xrandr \
-    --dpi 96 \
-    --output HDMI1  --mode 1920x1080 --pos 0x1920 --rotate normal --rate 60 --brightness 1 \
-    --mode 1920x1080 --pos 1417x0 --rotate right --rate 50 --brightness 0.8 \
-    --output HDMI2 --off \
-    --output DP1 --off \
-    --output HDMI2 --off  \
-    --output VIRTUAL1 --off
+#!/bin/sh
+# Configure the primary display (eDP1)
+xrandr --output eDP1 --primary --mode 1920x1080 --pos 1080x350 --rotate normal --brightness 1 
+
+# Disable DP1
+xrandr --output DP1 --off
+
+# Configure HDMI1 with rotation to the right
+xrandr --output HDMI1 --mode 1920x1080 --pos 0x0 --rotate right --brightness 1  
+
+# Disable HDMI2
+xrandr --output HDMI2 --off
+
+# Disable VIRTUAL1
+xrandr --output VIRTUAL1 --off
