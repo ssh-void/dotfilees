4f5b79ea258bb469063c734c020df6123128de17
ssh-void <168687039+ssh-void@users.noreply.github.com>
2024-12-11 19:31:58 +0100

    Update config.def.h

--- .config/.suckless/dwm/config.def.h
+++ .config/.suckless/dwm/config.def.h
@@ -59,8 +59,8 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char  *dmenucmd[] = { "dmenu_run", NULL };
-static const char *termcmd[]  = { "st",NULL };
+static const char *dmenucmd[] = { "dmenu_run",NULL };
+static const char *termcmd[]  = { "alacritty",NULL };
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
