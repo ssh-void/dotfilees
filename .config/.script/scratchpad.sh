4024b3ce1a71331feb8af957b56ddee3f5ec949e
ssh-void <168687039+ssh-void@users.noreply.github.com>
2024-12-16 03:35:13 +0100

    Update scratchpad.sh

--- .config/.script/scratchpad.sh
+++ .config/.script/scratchpad.sh
@@ -1,6 +1,6 @@
 winclass="$(xdotool search --classname scpad)" # i xdo xdotool
 if [ -z "$winclass" ]; then
-	st -f "Ubuntu Mono:size=15:weight=SemiBold" -g "90x30" -n "scpad" &>/dev/null &
+	st -f "Iosevka:size=13:weight=Regular" -g "100x30" -n "scpad" &>/dev/null &
 else
     if [ ! -f /tmp/scpad ]; then
 	touch /tmp/scpad && xdo hide "$winclass"
