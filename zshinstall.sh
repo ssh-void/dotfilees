5a70b5268a436c7cefd4f18698bdc9133a1158c8
drakvoid <draklinux@protonmail.com>
2024-12-16 03:51:01 +0100

    constomosation

---
+++ zshinstall.sh
@@ -0,0 +1,2 @@
+sudo xbps-install -Syu zsh zsh-completions zsh-autosuggestions zsh-syntax-highlighting	
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
