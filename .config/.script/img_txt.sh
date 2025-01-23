24983c26a2a12ada8b70dbb69b2e180eb90a3b8e
ssh-void <168687039+ssh-void@users.noreply.github.com>
2025-01-23 15:44:19 +0100

    Update img_txt.sh

--- .config/.script/img_txt.sh
+++ .config/.script/img_txt.sh
@@ -2,7 +2,7 @@
 #flameshot gui --raw | tesseract-ocr stdin stdout | xclip -in -selection clipboard 
 #maim -s | tesseract-ocr stdin stdout | xsel --clipboard --input 
 #flameshot gui --raw | tesseract-ocr stdin stdout | xsel --clipboard --input 
-maim -s -c 0.2706,0.2510,0.2392,1.0 -n 2 -b 2 | tesseract-ocr -l eng+fra+ara stdin stdout | xclip -selection clipboard &> /dev/null	
+maim -s -c 0.2706,0.2510,0.2392,1.0 -n 2 -b 2 | tesseract-ocr --oem 1 -l eng+fra+ara stdin stdout | xclip -selection clipboard &> /dev/null	
 #wait
 x=$(xclip -o -selection clipboard) 
 #brave-browser-stable "https://context.reverso.net/traduction/anglais-francais/$x" 
