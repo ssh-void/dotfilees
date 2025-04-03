f91955765b6c4773ccbd22a5bdf5df2caff9c9bf
ssh-void <168687039+ssh-void@users.noreply.github.com>
2025-04-03 02:22:25 +0000

    Update xbps-src.sh

--- script-bash/xbps-src.sh
+++ script-bash/xbps-src.sh
@@ -1,16 +1,15 @@
 sudo xbps-install git base-devel xtools python3
-#=============================================================#
+#===================================================================================#
 cd /opt/
 sudo git clone --depth=1 https://github.com/void-linux/void-packages.git
-sudo chown -R $(whoami):$(whoami) .
-#sudo chown -R $(whoami):$(whoami) .
+cd /opt/ && sudo chown -R $(whoami):$(whoami) .
 #git clean -fd && git reset --hard && git pull
-#=============================================================#
+#====================================================================================#
 cd /opt/void-packages && ./xbps-src binary-bootstrap # xbps-src cannot be used as root
 echo XBPS_ALLOW_RESTRICTED=yes >> etc/conf
 #echo XBPS_CHROOT_CMD=uchroot >> etc/conf
 #echo XBPS_CHROOT_CMD_ARGS=-t >> etc/conf
-#=============================================================#
+#====================================================================================#
 cd /opt/void-packages && ./xbps-src pkg torbrowser-launcher && xi torbrowser-launcher
 #./xbps-src pkg google-chrome &&  xi google-chrome
 #./xbps-src pkg discord && xi discord
