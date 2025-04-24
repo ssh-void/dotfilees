36c16fe7cfc354a2e0f7a3ec67eb8ed73e032e6d
Your Name <you@example.com>
2025-04-24 21:05:33 +0100

    ......

--- .config/.script/img_txt.sh
+++ .config/.script/img_txt.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
 
-#flameshot gui --raw | tesseract-ocr stdin stdout | xclip -in -selection clipboard 
-#maim -s | tesseract-ocr stdin stdout | xsel --clipboard --input 
-#flameshot gui --raw | tesseract-ocr stdin stdout | xsel --clipboard --input 
+# flameshot gui --raw | tesseract-ocr stdin stdout | xclip -in -selection clipboard 
+# maim -s | tesseract-ocr stdin stdout | xsel --clipboard --input 
+# flameshot gui --raw | tesseract-ocr stdin stdout | xsel --clipboard --input 
+
 maim -s -c 0.2706,0.2510,0.2392,1.0 -n 2 -b 2 | tesseract-ocr --oem 1 -l eng+fra+rus+ara stdin stdout | xclip -selection clipboard &> /dev/null	
-#wait
 x=$(xclip -o -selection clipboard) 
-#brave-browser-stable "https://context.reverso.net/traduction/anglais-francais/$x" 
-#firefox "https://context.reverso.net/traduction/anglais-francais/$x" 
-#brave-browser-stable "https://www.deepl.com/fr/translator#en/fr/$x" 
-#firefox "https://www.deepl.com/fr/translator#en/fr/$x" 
