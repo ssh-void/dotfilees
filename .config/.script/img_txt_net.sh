787d5b4b89848ad4d81cfe6ddcd4c57e735ba9ac
Your Name <you@example.com>
2025-04-24 21:18:34 +0100

    ......

--- .config/.script/img_txt_net.sh
+++ .config/.script/img_txt_net.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 
-#flameshot gui --raw | tesseract-ocr stdin stdout | xclip -in -selection clipboard 
-#maim -s | tesseract-ocr stdin stdout | xsel --clipboard --input 
-#flameshot gui --raw | tesseract-ocr stdin stdout | xsel --clipboard --input 
+# flameshot gui --raw | tesseract-ocr stdin stdout | xclip -in -selection clipboard &> /dev/null 
+# maim -s -c 0.2706,0.2510,0.2392,1.0 -n 2 -b 2 | tesseract-ocr stdin stdout | xsel --clipboard --input &> /dev/null 
+# flameshot gui --raw | tesseract-ocr stdin stdout | xsel --clipboard --input &> /dev/null 
+
 maim -s -c 0.2706,0.2510,0.2392,1.0 -n 2 -b 2  | tesseract-ocr stdin stdout | xclip -selection secondary &> /dev/null 
-#wait
+
 x=$(xclip -o -selection secondary) 
-#brave-browser-stable "https://context.reverso.net/traduction/anglais-francais/$x" 
-#firefox "https://context.reverso.net/traduction/anglais-francais/$x" 
-#brave-browser-stable "https://www.deepl.com/fr/translator#en/fr/$x" 
-firefox "https://www.deepl.com/fr/translator#en/fr/$x" 
+
+BROWSER="firefox"
+#BROWSER="brave-browser-stable"
+
+#$BROWSER  "https://context.reverso.net/traduction/anglais-francais/$x" 
+#$BROWSER  "https://context.reverso.net/traduction/anglais-francais/$x" 
+#$BROWSER  "https://www.deepl.com/fr/translator#en/fr/$x" 
+$BROWSER "https://www.deepl.com/fr/translator#en/fr/$x" 
