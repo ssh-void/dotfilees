1de42dabe79843e6cbe9c1bdb51c90bdd50e949c
Your Name <you@example.com>
2025-04-28 04:39:18 +0100

    site eng..

--- .config/.script/siteen.sh
+++ .config/.script/siteen.sh
@@ -5,12 +5,26 @@ x=$(xclip -o -selection primary)
 #BROWSER="firefox"
 BROWSER="brave-browser-stable"
 
-https://context.reverso.net/traduction/anglais-francais/good
-https://www.deepl.com/fr/translator#en/fr/good%0A
-https://www.wordreference.com/enfr/good
-https://www.dictionary.com/
-https://dictionary.cambridge.org/dictionary/english-arabic/shame?q=+shame
-https://www.merriam-webster.com/dictionary/good
-https://www.the-conjugation.com/english/verb/do.php
-https://www.oxfordlearnersdictionaries.com/
-https://www.linguee.com/english-french/search?source=auto&query=good
+$BROWSER "https://context.reverso.net/traduction/anglais-francais/$x"
+$BROWSER "https://www.deepl.com/fr/translator#en/fr/$x%0A"
+$BROWSER "https://translate.google.com/?hl=fr&sl=en&tl=fr&text=$x%0A&op=translate"
+$BROWSER "https://www.wordreference.com/enfr/$x"
+$BROWSER "https://dictionary.cambridge.org/dictionary/english-arabic/$x"
+$BROWSER "https://www.online-translator.com/traduction/anglais-fran%C3%A7ais/$x"
+$BROWSER "https://translate.yandex.com/?source_lang=en&target_lang=fr&text=$x"
+$BROWSER "https://www.almaany.com/en/dict/ar-en/$x"
+$BROWSER "https://enfr.dict.cc/?s=$x"
+$BROWSER "https://www.linguee.com/english-french/search?source=auto&query=$x"
+$BROWSER "https://ninjawords.com/$x"
+$BROWSER "https://www.urbandictionary.com/define.php?term=$x"
+$BROWSER "https://dictzone.com/english-arabic-dictionary/$x"
+$BROWSER "https://laratranslate.com/traduction?source=en&target=fr&text=$x&style=Faithful"
+$BROWSER "https://www.dictionary.com/browse/$x"
+$BROWSER "https://www.thesaurus.com/browse/$x"
+$BROWSER https://dictionary.cambridge.org/dictionary/english-arabic/shame?q=+shame
+$BROWSER "https://www.merriam-webster.com/dictionary/$x"
+$BROWSER https://www.the-conjugation.com/english/verb/do.php
+$BROWSER https://www.oxfordlearnersdictionaries.com/
+$BROWSER https://www.linguee.com/english-french/search?source=auto&query=good
+$BROWSER "https://www.quodb.com/search/$x?advance-search=false&keywords=$x"
+
