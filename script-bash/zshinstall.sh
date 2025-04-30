7dd69b00d7e71c37f3d986fc9e9732601c596020
Your Name <you@example.com>
2025-04-30 01:01:42 +0100

    aliases..

--- script-bash/zshinstall.sh
+++ script-bash/zshinstall.sh
@@ -1,6 +1,7 @@
 #sudo xbps-install -Syu zsh zsh-completions zsh-autosuggestions zsh-syntax-highlighting zsh-history-substring-search
 #sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 # Installing zsh
+sudo xbps-install -Syu zsh
 mkdir ~/.oh-my-zsh &> /dev/null
 git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh &> /dev/null
 
