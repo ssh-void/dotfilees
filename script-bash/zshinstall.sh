b80ba9dab56b6ac722b4f3833245f172f4ee3eb3
Your Name <you@example.com>
2025-04-30 01:01:18 +0100

    aliases..

--- script-bash/zshinstall.sh
+++ script-bash/zshinstall.sh
@@ -1,2 +1,10 @@
-sudo xbps-install -Syu zsh zsh-completions zsh-autosuggestions zsh-syntax-highlighting zsh-history-substring-search
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+#sudo xbps-install -Syu zsh zsh-completions zsh-autosuggestions zsh-syntax-highlighting zsh-history-substring-search
+#sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# Installing zsh
+mkdir ~/.oh-my-zsh &> /dev/null
+git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh &> /dev/null
+
+# Cloning some awesome plugins 
+cd ~/.oh-my-zsh/custom/plugins && git clone https://github.com/zsh-users/zsh-autosuggestions.git &> /dev/null
+cd ~/.oh-my-zsh/custom/plugins && git clone https://github.com/zsh-users/zsh-syntax-highlighting.git &> /dev/null
+cd ~/.oh-my-zsh/custom/plugins && git clone https://github.com/zsh-users/zsh-history-substring-search.git 2>/dev/null
