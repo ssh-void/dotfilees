b93a6421147aa2e258f0eab057ff314d57a1ce69
Your Name <you@example.com>
2025-04-30 17:57:04 +0100

    aliases..

--- script-bash/pipenv.sh
+++ script-bash/pipenv.sh
@@ -1,1 +1,11 @@
+#!/usr/bin/env bash
+
 cd ~ && python3 -m venv .env
+source /home/${USER}/.env/bin/activate
+pip install --upgrade pip
+pip install --upgrade instaloader 
+pip install -U --pre "yt-dlp[default]"
+pip install --upgrade telethon
+pip install --upgrade gTTS
+sudo ln -s $HOME/.env/bin/yt-dlp $HOME/.local/bin/
+sudo ln -s $HOME/.env/bin/instaloader $HOME/.local/bin/
