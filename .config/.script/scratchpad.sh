e4d444be3fb5847c33f112e02796952cac52235d
ssh-void <168687039+ssh-void@users.noreply.github.com>
2025-05-03 15:22:20 +0100

    Update scratchpad.sh

--- .config/.script/scratchpad.sh
+++ .config/.script/scratchpad.sh
@@ -2,9 +2,9 @@
 
 winclass="$(xdotool search --classname scpad)" # i xdo xdotool
 if [ -z "$winclass" ]; then
-	st -f "Iosevka:size=14:weight=Regular" -g "100x30" -n "scpad" -e tmux -f ~/.config/tmux/tmux.conf &>/dev/null 
-       # st -f "Iosevka:size=14:weight=Regular" -g "120x33" -n "scpad" -e tmux -f ~/.config/tmux/tmux.conf &>/dev/null 
-       # st -f "Iosevka:size=16:weight=Regular" -g "150x40" -n "scpad" -e tmux -f ~/.config/tmux/tmux.conf &>/dev/null 
+	st -f "Iosevka:size=14:weight=Regular" -g "100x30" -n "scpad" -e tmux &>/dev/null 
+       # st -f "Iosevka:size=14:weight=Regular" -g "120x33" -n "scpad" -e tmux &>/dev/null 
+       # st -f "Iosevka:size=16:weight=Regular" -g "150x40" -n "scpad" -e tmux &>/dev/null 
 else
     if [ ! -f /tmp/scpad ]; then
 	touch /tmp/scpad && xdo hide "$winclass"
