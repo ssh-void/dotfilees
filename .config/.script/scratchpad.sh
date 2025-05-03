a4dc377c58e9cbfbd18895304bf340681c662b49
ssh-void <168687039+ssh-void@users.noreply.github.com>
2025-05-03 15:35:01 +0100

    Update scratchpad.sh

--- .config/.script/scratchpad.sh
+++ .config/.script/scratchpad.sh
@@ -2,7 +2,7 @@
 
 winclass="$(xdotool search --classname scpad)" # i xdo xdotool
 if [ -z "$winclass" ]; then
-      st -f "Iosevka:size=14:weight=Regular" -g "122x33+286+58" -n "scpad" -e "tmux" &>/dev/null # xwininfo
+      st -f "Iosevka:size=14:weight=Regular" -g "122x33+295+49" -n "scpad" -e "tmux" &>/dev/null # xwininfo
 else
     if [ ! -f /tmp/scpad ]; then
 	touch /tmp/scpad && xdo hide "$winclass"
