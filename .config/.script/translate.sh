11938ff8de616f086bff1cad24a3960feb6ac3be
Your Name <you@example.com>
2025-05-05 00:00:03 +0100

    sdvsdv

--- .config/.script/translate.sh
+++ .config/.script/translate.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-BROWSER="firefox"
-#BROWSER="brave-browser-stable"
+#BROWSER="firefox"
+BROWSER="brave-browser-stable"
 
 x=$(xclip -o sel)
 
 $BROWSER "https://www.deepl.com/fr/translator#en/fr/$x"
 $BROWSER "https://context.reverso.net/traduction/anglais-francais/$x" 
-$BROWSER "https://context.reverso.net/traduction/anglais-francais/$x" 
+$BROWSER "https://www.reverso.net/traduction-texte#sl=eng&tl=fra&text=$x"
+$BROWSER "https://translate.yandex.com/?source_lang=en&target_lang=fr&text=$x"
+$BROWSER "https://laratranslate.com/traduction?source=en&target=fr&text=$x"
