af166f61d7f92f41f3aceb4e568c1f703c3528ce
Your Name <you@example.com>
2025-05-05 04:51:03 +0100

    script mount ext4

---
+++ .config/mount/mountext4.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+set -e
+doas mkdir -p /mnt/ssd/
+doas mount -t ext4 -o defaults UUID=622427a0-7005-4a2d-bbbf-e9264438ac62 /mnt/ssd/
+doas chown $USER:$USER /mnt/ssd/
---
+++ .config/mount/umountext4.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+set -e
+doas umount /mnt/ssd 
+sleep 1 
+doas rm -rf /mnt/ssd/
