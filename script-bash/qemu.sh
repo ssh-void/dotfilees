4c88d48224167d6a78345579d660921056c9bcaf
ssh-void <168687039+ssh-void@users.noreply.github.com>
2025-05-06 12:00:02 +0100

    Update qemu.sh

--- script-bash/qemu.sh
+++ script-bash/qemu.sh
@@ -1,1 +1,2 @@
-sudo xbps-install -Syu qemu qemu-common qemu-firmware qemu-img qemu-system-amd64
+# sudo xbps-install -Syu qemu qemu-common qemu-firmware qemu-img qemu-system-amd64
+sudo xbps-install -Syu qemu-system-amd64
