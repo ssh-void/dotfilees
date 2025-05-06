412bfb409785e72b96f7f56dc197ad73e98b6d37
ssh-void <168687039+ssh-void@users.noreply.github.com>
2025-05-06 14:38:49 +0100

    Update xorg-minimal.sh

--- script-bash/xorg-minimal.sh
+++ script-bash/xorg-minimal.sh
@@ -1,1 +1,1 @@
-sudo xbps-install -Syu base-devel xorg-server xinit xorg-input-drivers xrandr xrdb noto-fonts-cjk noto-fonts-emoji noto-fonts-ttf
+sudo xbps-install -Syu xorg-server xinit xorg-input-drivers xrandr xrdb xdo xdotool
