4d8629bc1d98add3fe17d0b5d1416164d5442535
ssh-void <168687039+ssh-void@users.noreply.github.com>
2025-05-06 12:19:33 +0100

    Update firefox.sh

--- script-bash/firefox.sh
+++ script-bash/firefox.sh
@@ -1,1 +1,3 @@
-sudo xbps-install -Syu firefox firefox-i18n-ar firefox-i18n-en-US firefox-i18n-fr speech-dispatcher
+sudo xbps-install -Syu firefox firefox-i18n-ar firefox-i18n-en-US firefox-i18n-fr firefox-i18n-ru  firefox-i18n-zh-CN firefox-i18n-zh-TW speech-dispatcher
+# firefox
+sudo ln -s /usr/share/fontconfig/conf.avail/70-no-bitmaps.conf /etc/fonts/conf.d/
