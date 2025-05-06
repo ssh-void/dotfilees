ecc8e5ab3ef0de80d889077e1a7c263c2db68c69
ssh-void <168687039+ssh-void@users.noreply.github.com>
2025-05-06 14:03:15 +0100

    Update intel.sh

--- script-bash/intel.sh
+++ script-bash/intel.sh
@@ -1,1 +1,10 @@
 
+#intel 
+sudo xbps-install -Syu intel-ucode # nonfree 
+sudo xbps-install -Syu xf86-video-intel # free 
+sudo xbps-install -Syu linux-firmware-intel # free 
+sudo xbps-install -Syu intel-video-accel # free 
+sudo xbps-install -Syu intel-media-driver # free 
+sudo xbps-install -Syu libva-intel-driver # free 
+sudo xbps-install -Syu mesa-vulkan-intel # free
+sudo xbps-install -Syu mesa-vaapi # free # video acceleration Intel
