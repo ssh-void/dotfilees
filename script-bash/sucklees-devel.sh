8ae4c5f34e010e48c5727107e9f849db003b891a
ssh-void <168687039+ssh-void@users.noreply.github.com>
2025-05-06 14:49:16 +0100

    Update sucklees-devel.sh

--- script-bash/sucklees-devel.sh
+++ script-bash/sucklees-devel.sh
@@ -1,1 +1,6 @@
 sudo xbps-install -Syu libX11-devel libXft-devel libXinerama-devel libXrandr-devel
+# nsxiv
+sudo xbps-install imlib2-devel
+sudo xbps-install libX11-devel imlib2-devel libXinerama-devel libXft-devel
+sudo xbps-install inotify-tools-devel
+sudo xbps-install libexif-devel
