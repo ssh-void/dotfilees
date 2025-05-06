80e5eb052ce765355c7146e5071478919666daef
ssh-void <168687039+ssh-void@users.noreply.github.com>
2025-05-06 12:03:53 +0100

    Update qemu.sh

--- script-bash/qemu.sh
+++ script-bash/qemu.sh
@@ -1,2 +1,7 @@
 # sudo xbps-install -Syu qemu qemu-common qemu-firmware qemu-img qemu-system-amd64
 sudo xbps-install -Syu qemu-img qemu-system-amd64 qemu-common qemu-firmware
+sudo usermod -aG kvm $(whoami) 
+sudo modprobe kvm-intel 
+cd /var/lib/libvirt/images && sudo qemu-img create -f qcow2 -o preallocation=off 50-voidlinux.qcow2 50G # tested
+cd /var/lib/libvirt/images && sudo qemu-img create -f qcow2 -o preallocation=off 20-voidlinux.qcow2 20G # tested
+cd /var/lib/libvirt/images && sudo qemu-img create -f qcow2 -o preallocation=off 70-voidlinux.qcow2 70G # tested
