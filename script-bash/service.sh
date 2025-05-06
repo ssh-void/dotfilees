2a409655f624af34dfdb44f6d81d64f408ba763f
ssh-void <168687039+ssh-void@users.noreply.github.com>
2025-05-06 14:39:54 +0100

    Update service.sh

--- script-bash/service.sh
+++ script-bash/service.sh
@@ -1,6 +1,6 @@
 sudo rm /var/service/agetty-tty{3,4,5,6}
-#sudo rm /var/service/wpa_supplicant
-#sudo rm /var/service/dhcpcd
+sudo rm /var/service/wpa_supplicant
+sudo rm /var/service/dhcpcd
 sudo ln -s /etc/sv/crond /var/service
 sudo ln -s /etc/sv/acpid/ /var/service/
 sudo ln -s /etc/sv/bluetoothd/ /var/service/
@@ -8,7 +8,7 @@ sudo ln -s /etc/sv/chronyd/ /var/service/
 sudo ln -s /etc/sv/crond/ /var/service/
 sudo ln -s /etc/sv/dbus/ /var/service/
 sudo ln -s /etc/sv/libvirtd/ /var/service/
-#sudo ln -s /etc/sv/NetworkManager/ /var/service/
+sudo ln -s /etc/sv/NetworkManager/ /var/service/
 sudo ln -s /etc/sv/sshd/ /var/service/
 sudo ln -s /etc/sv/tor/ /var/service/
 sudo ln -s /etc/sv/udevd/ /var/service/
