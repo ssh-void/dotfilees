51d128510ce68b9df3697fc68581fb3ad2505ffb
ssh-void <168687039+ssh-void@users.noreply.github.com>
2025-05-08 03:21:29 +0100

    Update nvidia-install.sh

--- script-bash/nvidia-install.sh
+++ script-bash/nvidia-install.sh
@@ -1,7 +1,8 @@
+sudo xbps-install -Syu xorg-server-devel  # !sudo nvidia-xconfig
 sudo xbps-install -Syu void-repo-nonfree 
-sudo xbps-install -Syu nvidia  # nonfree 
+sudo xbps-install -Syu nvidia  # current 
 sudo xbps-install -Syu nvidia-opencl # nonfree 
-sudo xbps-install -Syu vulkan-loader # free  
-sudo xbps-install -Syu mesa-vdpau  # free #video acceleration nv
+sudo xbps-install -Syu vulkan-loader # current  
+sudo xbps-install -Syu mesa-vdpau  # current #video acceleration nv
 sudo xbps-install -Syu void-repo-nonfree # add void-repo-multilib-nonfree
 sudo xbps-install -Syu nv-codec-headers nvidia nvidia-opencl nvidia-vaapi-driver
