956a421cc54b66d349c591766b88712ac6719587
Your Name <you@example.com>
2025-05-12 00:46:39 +0100

    config...

--- .config/.script/scratchpad.sh
+++ .config/.script/scratchpad.sh
@@ -2,7 +2,8 @@
 
 winclass="$(xdotool search --classname scpad)" # i xdo xdotool
 if [ -z "$winclass" ]; then
-      st -f "Iosevka:size=14:weight=Regular" -g "122x33+295+49" -n "scpad" -e "tmux" &>/dev/null # xwininfo
+#             st -f "Iosevka:size=14:weight=Regular:pixelsize=22:antialias=true:autohint=true" -g "122x33+295+49" -n "scpad" -e "tmux" &>/dev/null  # xwininfo
+              st -f "Terminess Nerd Font Mono:style=Bold:pixelsize=22:antialias=true:autohint=true" -g "116x35+203+47" -n "scpad" -e "tmux" > /dev/null 2>&1 & # xwininfo
 else
     if [ ! -f /tmp/scpad ]; then
 	touch /tmp/scpad && xdo hide "$winclass"
