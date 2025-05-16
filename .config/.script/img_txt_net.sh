6bfa643b58f841e06b9fd98bf06bc5d26625d2d1
Your Name <you@example.com>
2025-05-16 03:07:23 +0100

    config...

--- .config/.script/img_txt_net.sh
+++ .config/.script/img_txt_net.sh
@@ -11,6 +11,9 @@ x=$(xclip -o -selection secondary)
 #BROWSER="firefox"
 #BROWSER="brave-browser-stable"
 
-$BROWSER  "https://context.reverso.net/traduction/anglais-francais/$x" 
-$BROWSER  "https://context.reverso.net/traduction/anglais-francais/$x" 
-$BROWSER "https://www.deepl.com/fr/translator#en/fr/$x" 
+$BROWSER "https://translate.google.com/?sl=auto&tl=fr&text=$x&op=translate/$x"
+$BROWSER "https://www.deepl.com/fr/translator#en/fr/$x"
+$BROWSER "https://context.reverso.net/traduction/anglais-francais/$x"
+$BROWSER "https://www.reverso.net/traduction-texte#sl=eng&tl=fra&text=$x"
+$BROWSER "https://translate.yandex.com/?source_lang=en&target_lang=fr&text=$x"
+$BROWSER "https://laratranslate.com/traduction?source=en&target=fr&text=$x"
