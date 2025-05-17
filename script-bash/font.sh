74e1c34cb4a51951e6dae53bf28ebcc51e1f2bf9
Your Name <you@example.com>
2025-05-17 23:11:08 +0100

    ...

--- script-bash/font.sh
+++ script-bash/font.sh
@@ -1,1 +1,3 @@
-sudo xbps-install -Syu font-inconsolata-otf noto-fonts-cjk noto-fonts-emoji noto-fonts-ttf ttf-ubuntu-font-family terminus-font
+sudo xbps-install -Syu font-inconsolata-otf
+sudo xbps-install -Syu noto-fonts-cjk noto-fonts-emoji
+sudo xbps-install -Syu noto-fonts-ttf
--- script-bash/python.sh
+++ script-bash/python.sh
@@ -1,1 +1,2 @@
-sudo xbps-install -Syu python-pip python python3-pip python2 python3 python3-dbus python3-devel
+sudo xbps-install -Syu python-pip 
+sudo xbps-install -Syu python python3-pip python2 python3 python3-dbus python3-devel
