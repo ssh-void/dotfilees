f6c3097da97c1a7102cddfd1bcf1b6c31a0a6240
ssh-void <168687039+ssh-void@users.noreply.github.com>
2025-05-31 21:21:12 +0100

    Update docker.sh

--- script-bash/docker.sh
+++ script-bash/docker.sh
@@ -1,7 +1,8 @@
+USERNAME=$(logname)
 sudo xbps-install -Syu docker
 #sudo xbps-install -Syu nvidia-docker
 doas ln -s /etc/sv/docker /var/service/
-doas usermod -aG docker sh  # pour utilise docker sans sudo  [ reboot]
+doas usermod -aG docker "$USERNAME"
 doas sv status docker
 doas docker pull alpine
 #doas shutdown -r now
