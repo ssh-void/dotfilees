594ef290304253bbdc22b54ff9c6e5822595e7e6
ssh-void <168687039+ssh-void@users.noreply.github.com>
2025-06-01 01:22:50 +0100

    Update path.sh

--- script-bash/path.sh
+++ script-bash/path.sh
@@ -1,3 +1,7 @@
-mkdir  $HOME/programming
-mkdir  $HOME/programming/pyhton && mkdir  $HOME/programming/shell
-mkdir $HOME/Images/
+echo "mkdir Videos Image ... =========================="
+cd ~
+mkdir $HOME/programming
+mkdir $HOME/programming/pyhton && mkdir $HOME/programming/shell
+mkdir -p Videos Images Downloads Documents Music
+mkdir -p $HOME/Downloads/{torrent,firefox,brave,ytdlp/{audio,video}}
+clear
