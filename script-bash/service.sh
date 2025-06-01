068a0e8043647cb761911eccb966e53a75d6b982
ssh-void <168687039+ssh-void@users.noreply.github.com>
2025-06-01 23:07:42 +0100

    Update service.sh

--- script-bash/service.sh
+++ script-bash/service.sh
@@ -7,11 +7,11 @@ sudo ln -s /etc/sv/bluetoothd/ /var/service/
 sudo ln -s /etc/sv/chronyd/ /var/service/
 sudo ln -s /etc/sv/crond/ /var/service/
 sudo ln -s /etc/sv/dbus/ /var/service/
-sudo ln -s /etc/sv/libvirtd/ /var/service/
 sudo ln -s /etc/sv/NetworkManager/ /var/service/
 sudo ln -s /etc/sv/sshd/ /var/service/
 sudo ln -s /etc/sv/tor/ /var/service/
 sudo ln -s /etc/sv/udevd/ /var/service/
 sudo ln -s /etc/sv/ufw/ /var/service/
-sudo ln -s /etc/sv/virtlogd/ /var/service/
 sudo ln -s /etc/sv/zramen/ /var/service/
+sudo ln -s /etc/sv/virtlogd/ /var/service/
+sudo ln -s /etc/sv/libvirtd/ /var/service/
