8bd9f830efb2e327740308503d4231c8888086f1
ssh-void <168687039+ssh-void@users.noreply.github.com>
2025-06-01 23:13:46 +0100

    Update qemu.sh

--- script-bash/qemu.sh
+++ script-bash/qemu.sh
@@ -5,3 +5,5 @@ sudo modprobe kvm-intel
 cd /var/lib/libvirt/images && sudo qemu-img create -f qcow2 -o preallocation=off 50-voidlinux.qcow2 50G # tested
 cd /var/lib/libvirt/images && sudo qemu-img create -f qcow2 -o preallocation=off 20-voidlinux.qcow2 20G # tested
 cd /var/lib/libvirt/images && sudo qemu-img create -f qcow2 -o preallocation=off 70-voidlinux.qcow2 70G # tested
+sudo ln -s /etc/sv/virtlogd/ /var/service/
+sudo ln -s /etc/sv/libvirtd/ /var/service/
