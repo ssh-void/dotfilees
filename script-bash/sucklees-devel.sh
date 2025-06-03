987f396ebc3677a959011964ca05c85cb6473172
ssh-void <draklinux@protonmail.com>
2025-06-03 04:18:38 +0100

    ###...

--- script-bash/sucklees-devel.sh
+++ script-bash/sucklees-devel.sh
@@ -1,8 +1,8 @@
-sudo xbps-install -Syu libX11-devel libXft-devel libXinerama-devel libXrandr-devel make gcc pkg-config  #  pkg-config     !!   
+sudo xbps-install -Syu libX11-devel libXft-devel libXinerama-devel libXrandr-devel make gcc pkg-config  #  pkg-config     !!
 # sudo xbps-remove -Rv libXft-devel libXinerama-devel libXrandr-devel make gcc # (dwm )
 # nsxiv
-sudo xbps-install imlib2-devel
+sudo xbps-install imlib2-devel # libImlib2
 sudo xbps-install imlib2-devel libXinerama-devel libXft-devel
 sudo xbps-install inotify-tools-devel
 sudo xbps-install libexif-devel
-# sudo xbps-remove -Rv libXft-devel libXinerama-devel libXrandr-devel make gcc imlib2-devel imlib2-devel libXinerama-devel libXft-devel inotify-tools-devel libexif-deve # (nsxiv)
+# sudo xbps-remove -Rv libXft-devel libXinerama-devel libXrandr-devel make gcc libXinerama-devel libXft-devel inotify-tools-devel libexif-deve # (nsxiv)
--- script-bash/xbps-src.sh
+++ script-bash/xbps-src.sh
@@ -1,18 +1,14 @@
 USERNAME=$(logname)
-sudo xbps-install git base-devel xtools-minimal python3
+doas xbps-install git base-devel xtools-minimal python3
 echo "#===================================================================================#"
-cd /opt/ && sudo git clone --depth=1 https://github.com/garabik/grc.git
-cd /opt/ && sudo git clone --depth=1 https://github.com/void-linux/void-packages.git
-cd /opt/ && sudo chown -R "$USERNAME":"$USERNAME" void-packages
-cd grc/
-sudo ./install.sh
-sudo cp /etc/profile.d/grc.sh /etc/
+cd /opt/ && doas git clone --depth=1 https://github.com/void-linux/void-packages.git
+cd /opt/ && doas chown -R "$USERNAME":"$USERNAME" void-packages
 #git clean -fd && git reset --hard && git pull
 echo "#====================================================================================#"
 cd /opt/void-packages && ./xbps-src binary-bootstrap # xbps-src cannot be used as root
 echo XBPS_ALLOW_RESTRICTED=yes >> etc/conf
-#echo XBPS_CHROOT_CMD=uchroot >> etc/conf
-#echo XBPS_CHROOT_CMD_ARGS=-t >> etc/conf
+echo XBPS_CHROOT_CMD=uchroot >> etc/conf
+echo XBPS_CHROOT_CMD_ARGS=-t >> etc/conf
 sudo usermod -a -G xbuilder void
 echo "#===================================================================================#"
 cd /opt/void-packages && ./xbps-src pkg torbrowser-launcher && xi torbrowser-launcher
