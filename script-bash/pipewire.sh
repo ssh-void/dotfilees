ea861b7396c5a1618ced62956d04a812d7df7e2b
ssh-void <draklinux@protonmail.com>
2025-06-17 16:17:36 +0100

    pipewire&&rtkit

--- script-bash/pipewire.sh
+++ script-bash/pipewire.sh
@@ -1,9 +1,11 @@
 doas xbps-install -Syu pipewire pamixer alsa-pipewire libjack-pipewire wireplumber
 doas xbps-install -Syu pavucontrol
 doas xbps-install -Syu util-linux
+doas xbps-install -Syu rtkit
 #doas xbps-install pipewire-devel wireplumber-devel
 # sv on
-doas ln -s /etc/sv/dbus/ /var/service/
+doas ln -s /etc/sv/dbus /var/service/
+doas ln -s /etc/sv/rtkit /var/service/
 doas mkdir -p /etc/pipewire/pipewire.conf.d
 doas ln -s /usr/share/examples/wireplumber/10-wireplumber.conf /etc/pipewire/pipewire.conf.d/
 doas mkdir -p /etc/alsa/conf.d
