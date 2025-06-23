fee0635f6093e0a28f8d1c258301e9bd3c906f7a
ssh-void <draklinux@protonmail.com>
2025-06-23 02:42:41 +0100

    bashroot&perllocale

---
+++ .config/.script/linuxsite.sh
@@ -0,0 +1,2 @@
+"https://www.youtube.com/@AdrienLinuxtricks/search?query=$x"
+"https://github.com/search?q=$x&type=repositories"
--- .config/.script/searchengine.sh
+++ .config/.script/searchengine.sh
@@ -5,4 +5,9 @@ x=$(xclip -o -selection primary)
 #BROWSER="brave-browser-stable"
 $BROWSER "https://duckduckgo.com/?q=$x&t=newext&atb=v484-6&ia=web"
 $BROWSER "https://duckduckgo.com/?q=$x&t=newext&atb=v484-6&ia=web"
-
+$BROWSER "https://yandex.com/search?text=$x"
+$BROWSER "https://www.ecosia.org/search?method=index&q=$x"
+$BROWSER "https://www.bing.com/search?q=$x&lq=0&pq"
+$BROWSER "https://www.qwant.com/?q=$x&t=web"
+$BROWSER "https://search.inetol.net/search?q=$x"
+$BROWSER "https://search.brave.com/search?q=$x&source=web"
