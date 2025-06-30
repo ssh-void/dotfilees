07f730c1968d26435389dbb2964d28cab2c91a3d
ssh-void <draklinux@protonmail.com>
2025-06-30 21:39:56 +0100

    script xdg-*

--- script-bash/xdgmime.sh
+++ script-bash/xdgmime.sh
@@ -1,3 +1,4 @@
+sudo xbps-install -Syu xdg-utils
 xdg-mime default firefox.desktop text/html
 xdg-mime default firefox.desktop x-scheme-handler/http
 xdg-mime default firefox.desktop x-scheme-handler/https
