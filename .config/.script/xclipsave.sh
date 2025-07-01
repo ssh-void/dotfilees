0bb8ad1e5d990c492ae2135c134e4184b972052b
ssh-void <draklinux@protonmail.com>
2025-07-01 15:56:43 +0100

    xclipsave.sh

--- .config/.script/xclipsave.sh
+++ .config/.script/xclipsave.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # File where copied items will be stored
-output_file="liste.txt"
+output_file=$(mktemp /tmp/xlipsave-XXXX.txt)
 
 # Initialize the clipboard with an empty value
 previous_clipboard=""
@@ -10,15 +10,13 @@ previous_clipboard=""
 while true; do
     # Get the current clipboard content
     current_clipboard=$(xclip -o -selection clipboard)
-
     # If the clipboard content has changed from the previous one
     if [[ "$current_clipboard" != "$previous_clipboard" && -n "$current_clipboard" ]]; then
         # Append the content to the file
-        echo "$current_clipboard" >> "$output_file" &> /dev/null
+        echo "$current_clipboard" >> "$output_file"
         # Update the previous clipboard variable
         previous_clipboard="$current_clipboard"
     fi
-
     # Wait 1 second before checking again
     sleep 1
 done
