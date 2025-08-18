79bf526c82ec3d377a739ed5b7fdc653c612ef5d
ssh-void <draklinux@protonmail.com>
2025-08-18 19:13:54 +0100

    xdg-*

--- script-bash/xdgmime.sh
+++ script-bash/xdgmime.sh
@@ -3,4 +3,4 @@ xdg-mime default nnn.desktop inode/directory
 xdg-mime default firefox.desktop text/html
 xdg-mime default firefox.desktop x-scheme-handler/http
 xdg-mime default firefox.desktop x-scheme-handler/https
-
+xdg-mime default vlc.desktop video/x-matroska
