6fdd30092f254a4cd20a259cc629bce68ef7eb1f
ssh-void <draklinux@protonmail.com>
2025-08-18 01:02:03 +0100

    pipen.sh ...

--- script-bash/pipenv.sh
+++ script-bash/pipenv.sh
@@ -4,12 +4,14 @@ mkdir -p /home/${USER}/.local/share/tools/
 cd /home/${USER}/.local/share/tools/ && uv venv .env
 cd /home/${USER}/.local/share/tools/ && source .env/bin/activate
 uv pip install --upgrade instaloader
+uv pip install --upgrade gallery-dl
 uv pip install --upgrade --pre "yt-dlp[default]"
 uv pip install --upgrade telethon
 uv pip install --upgrade gTTS
-#uv pip install coqui-tts
-#uv pip install gradio-client
+uv pip install --upgrade autosub
+uv pip install browser-cookie3
 ln -s /home/${USER}/.local/share/tools/.env/bin/yt-dlp $HOME/.local/bin/
 ln -s /home/${USER}/.local/share/tools/.env/bin/instaloader $HOME/.local/bin/
 ln -s /home/${USER}/.local/share/tools/.env/bin/gtts-cli $HOME/.local/bin/
 ln -s /home/${USER}/.local/share/tools/.env/bin/whisper $HOME/.local/bin/
+ln -s /home/${USER}/.local/share/tools/.env/bin/gallery-dl $HOME/.local/bin/
