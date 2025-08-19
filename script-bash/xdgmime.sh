75edc5809d23637223a0ae9f3a290bbb451210f0
ssh-void <draklinux@protonmail.com>
2025-08-19 14:34:15 +0100

    xdg-

--- script-bash/xdgmime.sh
+++ script-bash/xdgmime.sh
@@ -1,5 +1,7 @@
-sudo xbps-install -Syu xdg-utils firefox nnn
+doas xbps-install -Syu xdg-utils firefox nnn
 xdg-mime default nnn.desktop inode/directory
+xdg-settings set default-web-browser firefox.desktop
+#xdg-settings set default-web-browser brave-browser.desktop
 xdg-mime default firefox.desktop text/html
 xdg-mime default firefox.desktop x-scheme-handler/http
 xdg-mime default firefox.desktop x-scheme-handler/https
