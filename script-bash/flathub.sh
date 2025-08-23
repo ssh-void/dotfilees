49c5001a7aa9c85e79c1dc689fe7a74d17ac4842
ssh-void <draklinux@protonmail.com>
2025-08-23 01:07:28 +0100

    clean NAme...

--- script-bash/flathub.sh
+++ script-bash/flathub.sh
@@ -1,14 +1,16 @@
-#Install Flatpak
+#Install Flatpak : https://flatpak.org/setup/Void%20Linux
 doas xbps-install -Syu flatpak
 #Add the Flathub repository to Flatpak
 flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 #install kdenlive
-flatpak --user install flathub org.kde.kdenlive -y
+flatpak --user install --or-update flathub org.kde.kdenlive -y
 #install Cryptomator
-flatpak --user install flathub org.cryptomator.Cryptomator -y
+flatpak --user install --or-update flathub org.cryptomator.Cryptomator -y
 #install FreeTube [The Private YouTube Client]
-flatpak --user install flathub io.freetubeapp.FreeTube
+flatpak --user install --or-update flathub io.freetubeapp.FreeTube
+#install brave-Browser
+#flatpak --user install --or-update flathub com.brave.Browser
 # update
-doas flatpak update
+doas flatpak update -y
 #for run use the export $(dbus-launch)
 #flatpak run "$(flatpak list --app --columns=application | fzy)" &> /dev/null
