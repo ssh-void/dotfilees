604899e6d7e819c671e61ab2e294468e07df0ab7
ssh-void <draklinux@protonmail.com>
2025-08-25 22:26:00 +0100

    gten_clip.sh  & gten_clip.sh

---
+++ .config/.script/gten_clip.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Récupère texte du presse-papier, nettoie et lit
+text=$(xclip -o | sed 's/\[[^]]*\]//g; s/"//g')
+
+gtts-cli -l en --slow "$text" | oniux mpv --no-config --no-video \
+    --af-add="volume=2" \
+    --term-osd-bar \
+    --ytdl-format=bestaudio \
+    --user-agent="Mozilla/5.0 (X11; Linux x86_64; rv:142.0) Gecko/20100101 Firefox/142.0" \
+    --input-conf=<(echo -e "UP add speed 0.1\nDOWN add speed -0.1") -
+
