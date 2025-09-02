417663dc62d67e3b2ab02bf87d2bbea1c173ac03
ssh-void <draklinux@protonmail.com>
2025-09-02 17:52:00 +0100

    ntfs-3g Stable Read/Write NTFS Driver

--- .config/mount/mountntfs.sh
+++ .config/mount/mountntfs.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 set -e
-doas mkdir -p /mnt/hdd1 && doas mkdir -p /mnt/hdd2 && doas mount -t ntfs -o defaults,uid=1000,gid=1000,umask=022 UUID=7862B45062B4153A /mnt/hdd1 && doas mount -t ntfs -o defaults,uid=1000,gid=1000,umask=022 UUID=98DC2FDADC2FB184 /mnt/hdd2
+doas mkdir -p /mnt/hdd11 && doas mkdir -p /mnt/hdd22 && sleep 1 && doas mount -t ntfs-3g -o defaults,uid=1000,gid=1000,umask=022 UUID=7862B45062B4153A /mnt/hdd11 && sleep 1 && doas mount -t ntfs-3g -o defaults,uid=1000,gid=1000,umask=022 UUID=98DC2FDADC2FB184 /mnt/hdd22
