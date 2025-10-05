eb4cc8ce813d5e5ec48b37c5bbe24fdfeb5dc8de
ssh-void <draklinux@protonmail.com>
2025-10-05 10:25:59 +0100

    c

--- script-bash/bluetooth.sh
+++ script-bash/bluetooth.sh
@@ -3,4 +3,4 @@ doas xbps-install -Syu rfkill util-linux bluez blueman libspa-bluetooth
 doas rfkill unblock bluetooth
 doas useradd -G bluetooth ${USER}
 doas ln -s /etc/sv/bluetoothd/ /var/service/
-doas shutdown -r now
+#doas shutdown -r now
